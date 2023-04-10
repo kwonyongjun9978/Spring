387b8ae3984b41bc95c05574ea07277528a3b676
Jun <rnsuz74@naver.com>
2023-04-10 20:24:24 +0900

    Spring_Annotation_MyBatis

---
+++ chapter05_SpringMaven/src/user/service/UserDeleteService.java
@@ -0,0 +1,39 @@
+package user.service;
+
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import user.bean.UserDTO;
+import user.dao.UserDAO;
+
+@Service
+public class UserDeleteService implements UserService {
+	@Autowired
+	private UserDAO userDAO = null; 
+		
+//	public void setUserDAO(UserDAO userDAO) {
+//		this.userDAO = userDAO;
+//	}
+
+	@Override
+	public void execute() {
+		System.out.println();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("삭제 할 아이디 입력 : ");
+		String id = scan.next();
+		
+		//DB
+		UserDTO userDTO = userDAO.getUser(id);
+		
+		if(userDTO == null) {
+			System.out.println("찾고자하는 아이디가 없습니다.");
+			return;
+		}
+		
+		userDAO.delete(id);
+		System.out.println("DB의 내용을 삭제하였습니다.");
+	}
+}
