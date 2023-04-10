eb137323148c89e89de5444ff8f58eb34c7cf609
Jun <rnsuz74@naver.com>
2023-04-10 20:18:37 +0900

    Spring_XML_MyBatis

---
+++ chapter05/src/user/dao/UserDAOMyBatis.java
@@ -0,0 +1,44 @@
+package user.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.Setter;
+import user.bean.UserDTO;
+
+@Transactional
+public class UserDAOMyBatis implements UserDAO {
+	@Setter
+	private SqlSession sqlSession;
+	
+	//@Transactional
+	@Override
+	public void write(UserDTO userDTO) {
+		sqlSession.insert("userSQL.write", userDTO);
+	}
+
+	//@Transactional
+	@Override
+	public List<UserDTO> getUserList() {
+		return sqlSession.selectList("userSQL.getUserList");
+	}
+
+	@Override
+	public UserDTO getUser(String id) {
+		return sqlSession.selectOne("userSQL.getUser", id);
+	}
+
+	@Override
+	public void update(Map<String, String> map) {
+		sqlSession.update("userSQL.update", map);
+	}
+
+	@Override
+	public void delete(String id) {
+		sqlSession.delete("userSQL.delete", id);
+	}
+
+}
